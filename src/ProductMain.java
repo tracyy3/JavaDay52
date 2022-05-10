a4f7768cff2d3405f58f2d95d32769efcd28e83f
tracy <tracyyang336@gmail.com>
2022-05-10 14:31:04 -0400

    first commit

--- src/ProductMain.java
+++ src/ProductMain.java
@@ -1,7 +1,7 @@
 public class ProductMain {
 
     public static void main (String[] args) {
-        Product product= new Product("A Flower dozen", 20.0, 5);
+        Product product= new Product("A flower dozen", 20.0, 5);
 
         product.printProduct( );
         product.totalCost();
